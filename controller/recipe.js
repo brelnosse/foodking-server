3bd222a531699356e6327f28828b88f193b12881
brelnosse <brelnosse2@gmail.com>
2025-08-18 11:07:32 +0100

    création des routes pour filtrer les résultats

--- controller/recipe.js
+++ controller/recipe.js
@@ -21,3 +21,45 @@ exports.getRecipes = (req, res)=>{
     .then((recipes)=> res.status(200).json({recipes: recipes}))
     .catch((error)=> res.status(400).json({error}))
 }
+exports.getCategories = (req, res)=>{
+    Recipe.find()
+    .then((recipes)=>{
+        let categories = recipes.map((el)=>{
+            return el.category;
+        }) 
+        let filteredCat = []
+
+        for(let i = 0; i < categories.length; i++){
+            if(!filteredCat.includes(categories[i])){
+                filteredCat.push(categories[i]);
+            }
+        }
+
+        res.status(200).json({categories: categories})
+    })
+    .catch(recipes => res.status(400).json({error}))
+}
+
+exports.getRecipesByName = (req, res)=>{
+    Recipe.find()
+    .then((recipes)=>{
+        let values = recipes.map((el)=>{
+            if(el.title.startsWith(req.params.name) || el.title.endsWith(req.params.name) || el.title.split(' ').includes(req.params.name)){
+                return el;
+            }
+        })
+        values = values.filter((el)=>{
+            return el
+        })
+        res.status(200).json({recipes: values})
+    })
+    .catch(error => res.status(400).json({error}))
+}
+
+exports.getRecipe = (req, res)=>{
+    Recipe.findOne({_id: req.params.id})
+    .then((recipe)=> {
+        res.status(200).json({recipe: recipe})
+    })
+    .catch(error=> res.status(400).json({error}))
+}
--- route/recipe.js
+++ route/recipe.js
@@ -4,6 +4,9 @@ const recipeCtrl = require('../controller/recipe')
 const multer = require('../middleware/multer-config');
 const { checkAuth } = require('../middleware/auth');
 
-route.post('/create', checkAuth, multer, recipeCtrl.createRecipe)
-route.get('/recipes', recipeCtrl.getRecipes)
+route.post('/create', checkAuth, multer, recipeCtrl.createRecipe);
+route.get('/recipes', recipeCtrl.getRecipes);
+route.get('/recipes/categories', recipeCtrl.getCategories);
+route.get('/recipes/search/:name', recipeCtrl.getRecipesByName)
+route.get('/recipes/:id', recipeCtrl.getRecipe)
 module.exports = route;
